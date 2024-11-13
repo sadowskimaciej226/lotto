d243b23c979d288407faad2aa70db47ff0f43931
sadow <sadowskimaciej226@gmail.com>
2024-11-13 19:38:14 +0100

    implemeted working IntegrationConfiguration to override clock

--- integration/java/pl/lotto/BaseIntegrationTest.java
+++ integration/java/pl/lotto/BaseIntegrationTest.java
@@ -2,7 +2,6 @@ package pl.lotto;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,18 +14,19 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-
+import pl.lotto.domain.AdjustableClock;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
-@SpringBootTest(classes = LottoApplication.class)
+@SpringBootTest(classes = {LottoApplication.class, IntegrationConfiguration.class})
 @ActiveProfiles("integration")
 @AutoConfigureMockMvc
 @Testcontainers
 public class BaseIntegrationTest {
+
     public static final String WIRE_MOCK_HOST = "http://localhost";
 
     @Autowired
-    private MockMvc mockMvc;
+    public MockMvc mockMvc;
 
     @Container
     public static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
@@ -34,16 +34,19 @@ public class BaseIntegrationTest {
     @Autowired
     public ObjectMapper objectMapper;
 
-    @DynamicPropertySource
-    public static void propertyOverride(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-        registry.add("lotto.number-generator.http.client.config.uri", () -> WIRE_MOCK_HOST);
-        registry.add("lotto.number-generator.http.client.config.port", () -> wireMockServer.getPort());
-    }
+    @Autowired
+    public AdjustableClock clock;
+
     @RegisterExtension
     public static WireMockExtension wireMockServer = WireMockExtension.newInstance()
             .options(wireMockConfig().dynamicPort())
             .build();
 
+    @DynamicPropertySource
+    public static void propertyOverride(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+        registry.add("lotto.number-generator.http.client.config.port", () -> wireMockServer.getPort());
+        registry.add("lotto.number-generator.http.client.config.uri", () -> WIRE_MOCK_HOST);
+    }
 
 }
